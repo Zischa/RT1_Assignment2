f37b30872f95cd27af055e190819760df16c1eaf
root <amtfrancesca@libero.it>
2024-12-28 10:37:36 +0000

    Versione funzionante del target_client, adesso il client comunica il target al server in maniera corretta

--- scripts/target_client.py
+++ scripts/target_client.py
@@ -1,29 +1,29 @@
-#!/usr/bin/env python 3
+#!/usr/bin/env python
 
 import rospy
 import actionlib
 import actionlib.msg
+import assignment_2_2024.msg
 from geometry_msgs.msg import Point, Pose, Twist
 from nav_msgs.msg import Odometry
-from assignment_2_2024.msg import Vel, PlanningAction, PlanningGoal
 from actionlib_msgs.msg import GoalStatus
 
 class GoalHandler:
 	def __init__(self):
 		rospy.init_node('target_client')
-		self.pub = rospy.Publisher ("/pos_vel", Vel, queue_size = 1)
-		self.client = actionlib.SimpleActionClient('/reaching_goal', PlanningAction)
+		
+		self.client = actionlib.SimpleActionClient('/reaching_goal', assignment_2_2024.msg.PlanningAction)
 		self.client.wait_for_server()
 
 		
 	def set_goal(self):
-		rospy.Subscriber("/odom", Odometry, self.publish_position_velocity)
+		#rospy.Subscriber("/odom", Odometry, self.publish_position_velocity)
 		while not rospy.is_shutdown():
 			#get current pose of the target
 			target_pos_x = rospy.get_param('/des_pos_x')
 			target_pos_y = rospy.get_param('/des_pos_y')
 			#Create Goal Object
-			target_goal = PlanningGoal()
+			target_goal = assignment_2_2024.msg.PlanningGoal()
 			#pass it to action file
 			target_goal.target_pose.pose.position.x = target_pos_x
 			target_goal.target_pose.pose.position.y = target_pos_y
@@ -61,6 +61,9 @@ def main():
 	handler = GoalHandler()
 	handler.set_goal()
 	
-if __name== '__main__':
-	main()	
+if __name__== '__main__':
+	try:
+		main()
+	except rospy.ROSInterruptException:
+		pass
 				
