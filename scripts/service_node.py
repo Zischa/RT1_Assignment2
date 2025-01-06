14f45368f6a2aa180f3d16229ee47c64ccf7efba
root <amtfrancesca@libero.it>
2025-01-06 20:05:27 +0000

    Final version, updated launch file with xterm

--- scripts/service_node.py
+++ scripts/service_node.py
@@ -11,13 +11,17 @@ class FindLastTarget:
 		rospy.Service("target_srv", Target_srv, self.handle_target_srv)
 		rospy.loginfo("Service ready...\n")
 		
+		
 		#Responding to service by giving the user the last position of the target
-	def handle_target_srv(sel, request):
+	def handle_target_srv(self, request):
 		last_coordinate = Target_srvResponse()
 		last_coordinate.target_pos_x = rospy.get_param('/des_pos_x')
 		last_coordinate.target_pos_y = rospy.get_param('/des_pos_y')
 		
 		return last_coordinate
+		
+		#Printing on screen the coordinate of the target
+	
 
 if __name__ == '__main__':
 	try:
